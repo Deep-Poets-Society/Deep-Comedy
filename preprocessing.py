a33cebe3e1cb633c66150fb84fd792b2c93e8235
Sasso <pietro.peppo@gmail.com>
2021-08-03 18:00:13 +0200

    comments improvement

--- preprocessing.py
+++ preprocessing.py
@@ -24,9 +24,12 @@ def preprocess():
     processed_text = re.sub(r'\n', r'<end>\n<start>', processed_text)
     processed_text = re.sub('^', '<start>', processed_text)
     processed_text = re.sub('$', '<end>', processed_text)
+
+    # add <syl> token in y.csv, remove syllabification in X.csv
     x_text = re.sub(r'\|', '', processed_text)
     y_text = re.sub(r'\|', r'<syl>', processed_text)
 
+    # save files
     with open('res/X.csv', 'w+', encoding='utf-8') as file:
         file.writelines(x_text)
     with open('res/y.csv', 'w+', encoding='utf-8') as file:
