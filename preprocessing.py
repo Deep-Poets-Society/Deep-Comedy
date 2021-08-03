9c9e42d95b168b7c9da8ee0414d0e884380e4d8e
Sasso <pietro.peppo@gmail.com>
2021-08-03 17:54:38 +0200

    preprocessing

---
+++ preprocessing.py
@@ -0,0 +1,37 @@
+import re
+
+
+def preprocess():
+    with open('res/divina_commedia_raw.txt', 'r+', encoding='utf-8') as file:
+        raw_text = ''.join(file.readlines())
+    # remove capital letters
+    processed_text = raw_text.lower()
+    # substitute " ’" with " i"
+    processed_text = re.sub(r' ’', ' i', processed_text)
+    # remove punctuation
+    processed_text = re.sub(r'[,.;:\-—?!"’()\[\]«»“‟”\']', '', processed_text)
+    # remove cantos' headings
+    processed_text = re.sub(r'.* • canto .*', '', processed_text)
+    # remove lines' numbers
+    processed_text = re.sub(r'\n *\d* ', '\n', processed_text)
+    # remove multiple blank lines
+    processed_text = re.sub(r'\n+', '\n', processed_text)
+    # remove initial and last blank lines
+    processed_text = re.sub(r'(^\n)|(\n$)', '', processed_text)
+    # remove multiple spaces and transform spaces in <s>
+    processed_text = re.sub(r' +', '<s>', processed_text)
+    # add <start> and <end> tokens
+    processed_text = re.sub(r'\n', r'<end>\n<start>', processed_text)
+    processed_text = re.sub('^', '<start>', processed_text)
+    processed_text = re.sub('$', '<end>', processed_text)
+    x_text = re.sub(r'\|', '', processed_text)
+    y_text = re.sub(r'\|', r'<syl>', processed_text)
+
+    with open('res/X.csv', 'w+', encoding='utf-8') as file:
+        file.writelines(x_text)
+    with open('res/y.csv', 'w+', encoding='utf-8') as file:
+        file.writelines(y_text)
+
+
+if __name__ == '__main__':
+    preprocess()
