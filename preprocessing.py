4fc51b12f86d620aeef61e682da3221049b7ff26
Sasso <pietro.peppo@gmail.com>
2021-08-09 16:09:21 +0200

    cesura without special cases

--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import re
+import pickle
 
 
 def preprocess():
@@ -6,10 +7,8 @@ def preprocess():
         raw_text = ''.join(file.readlines())
     # remove capital letters
     processed_text = raw_text.lower()
-    # substitute ’ with '
-    processed_text = re.sub('’', '\'', processed_text)
-    # remove punctuation, but '
-    processed_text = re.sub(r'[,.;:\-—?!"’()\[\]«»“‟”]', '', processed_text)
+    # remove punctuation, but ’
+    processed_text = re.sub(r'[,.;:\-—?!"()\[\]«»“‟”]', '', processed_text)
     # remove cantos' headings
     processed_text = re.sub(r'.* • canto .*', '', processed_text)
     # remove lines' numbers
@@ -28,12 +27,86 @@ def preprocess():
     # add <syl> token in y.csv, remove syllabification in X.csv
     x_text = re.sub(r'\|', '', processed_text)
     y_text = re.sub(r'\|', r'<syl>', processed_text)
+    # generate y_cesura.csv
+    y_cesura_text = cesura(x_text, y_text)
 
     # save files
     with open('res/X.csv', 'w+', encoding='utf-8') as file:
         file.writelines(x_text)
     with open('res/y.csv', 'w+', encoding='utf-8') as file:
         file.writelines(y_text)
+    with open('res/y_cesura.csv', 'w+', encoding='utf-8') as file:
+        file.writelines(y_cesura_text)
+
+
+def cesura(text, syl_text):
+    dictionary = pickle.load(open('res/dantes_dictionary.pkl', 'rb'))
+    # add entries not present
+    dictionary.update({'aegypto': [((0, 3, -1, 0), 'ae|gyp|to', 1)]})
+    text = re.sub(r'(<start>)|(<end>)', '', text)
+    syl_text = re.sub(r'(<start><syl>)|(<end>)', '', syl_text)
+    lines = text.split('\n')
+    syl_lines = syl_text.split('\n')
+    for i, (line, syl_line) in enumerate(zip(lines, syl_lines)):
+        tonic_accents = []
+        counter = 0
+        words = line.split('<s>')
+        for w in words:
+            num_syl = dictionary[w][0][0][1]
+            pos_accent = dictionary[w][0][0][2]
+            tonic_accents += [False for _ in range(num_syl)]
+            tonic_accents[pos_accent - 1] = True
+        syllables = syl_line.split('<syl>')
+        if len(syllables) != len(tonic_accents):
+            for j, s in enumerate(syllables):
+                for _ in re.findall(r'<s>.', s):
+                    del tonic_accents[j]
+        line_w_cesura = ''
+        # cesura lirica
+        if tonic_accents[2] and tonic_accents[5] and syllables[3].endswith('<s>'):
+            line_w_cesura = '<syl>'.join(syllables[0:4]) + '<syl><c>' + '<syl>'.join(syllables[4:])
+        # hendecasyllable a maiore, cesura maschile
+        elif tonic_accents[5] and syllables[5].endswith('<s>'):
+            line_w_cesura = '<syl>'.join(syllables[0:6]) + '<syl><c>' + '<syl>'.join(syllables[6:])
+        # hendecasyllable a minore, cesura maschile
+        elif tonic_accents[3] and syllables[3].endswith('<s>'):
+            line_w_cesura = '<syl>'.join(syllables[0:4]) + '<syl><c>' + '<syl>'.join(syllables[4:])
+        # hendecasyllable a maiore, cesura femminile
+        elif tonic_accents[5] and syllables[6].endswith('<s>'):
+            line_w_cesura = '<syl>'.join(syllables[0:7]) + '<syl><c>' + '<syl>'.join(syllables[7:])
+        # hendecasyllable a minore, cesura femminile
+        elif tonic_accents[3] and syllables[4].endswith('<s>'):
+            line_w_cesura = '<syl>'.join(syllables[0:5]) + '<syl><c>' + '<syl>'.join(syllables[5:])
+        # hendecasyllable a maiore, cesura after a proparoxytone
+        elif tonic_accents[5] and syllables[7].endswith('<s>'):
+            line_w_cesura = '<syl>'.join(syllables[0:8]) + '<syl><c>' + '<syl>'.join(syllables[8:])
+        # hendecasyllable a minore, cesura after a proparoxytone
+        elif tonic_accents[3] and syllables[5].endswith('<s>'):
+            line_w_cesura = '<syl>'.join(syllables[0:6]) + '<syl><c>' + '<syl>'.join(syllables[6:])
+        # hendecasyllable a maiore with cesura femminile between two words joint by sinalefe
+        elif tonic_accents[5] and '<s>' in syllables[6]:
+            line_w_cesura = '<syl>'.join(syllables[0:6]) + '<syl>' + \
+                            re.sub(r'<s>(?=.)', r'<s><c>', syllables[6]) + \
+                            '<syl>' + '<syl>'.join(syllables[7:])
+        # hendecasyllable a minore with cesura femminile between two words joint by sinalefe
+        elif tonic_accents[3] and '<s>' in syllables[4]:
+            line_w_cesura = '<syl>'.join(syllables[0:4]) + '<syl>' + \
+                            re.sub(r'<s>(?=.)', r'<s><c>', syllables[4]) + \
+                            '<syl>' + '<syl>'.join(syllables[5:])
+        # hendecasyllable a maiore with cesura after a proparoxytone and between two words joint by sinalefe
+        elif tonic_accents[5] and '<s>' in syllables[7]:
+            line_w_cesura = '<syl>'.join(syllables[0:7]) + '<syl>' + \
+                            re.sub(r'<s>(?=.)', r'<s><c>', syllables[7]) + \
+                            '<syl>' + '<syl>'.join(syllables[8:])
+        # hendecasyllable a minore with cesura after a proparoxytone and between two words joint by sinalefe
+        elif tonic_accents[3] and '<s>' in syllables[5]:
+            line_w_cesura = '<syl>'.join(syllables[0:5]) + '<syl>' + \
+                            re.sub(r'<s>(?=.)', r'<s><c>', syllables[5]) + \
+                            '<syl>' + '<syl>'.join(syllables[6:])
+        # TODO: special cases (-mente, -zial and non-canonic hendecasyllable)
+        line_w_cesura = '<start><syl>' + line_w_cesura + '<end>'
+        syl_lines[i] = line_w_cesura
+    return '\n'.join(syl_lines)
 
 
 if __name__ == '__main__':
